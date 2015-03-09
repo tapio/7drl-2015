cce2b7cfc180d0580d13886a1a403a4d1ed21758
Tapio Vierros <tapiovierros@gmail.com>
2015-03-09 19:04:37 +0200

    Use window.onload and disable timer hacks.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
 var world, ui; // Globals
 
-(function() {
+window.onload = function() {
 function start() {
 	// Still add a small delay as otherwise Firefox,
 	// possibly WP8.1, seems fail on first try.
-	window.setTimeout(function() {
+	//window.setTimeout(function() {
 		$("#loading-text").innerHTML = "Initializing...";
 		world = new World();
 		var pl = new Actor(world.dungeon.start[0], world.dungeon.start[1]);
@@ -28,18 +28,18 @@ function start() {
 
 		// Another hack to make sure display is reset if other methods of
 		// detecting web font load fail
-		(function checkDimension() {
+		/*(function checkDimension() {
 			if (window.innerWidth < ui.display.getContainer().clientWidth) {
 				ui.resetDisplay();
 				window.setTimeout(checkDimension, 500);
 			}
-		})();
+		})();*/
 		$("#loading").style.display = "none";
 		$("#game").style.display = "block";
-	}, 100);
+	//}, 100);
 }
 
 $("#loading-text").innerHTML = "Waiting for a font...";
 
-FontDetect.onFontLoaded(CONFIG.fontFamily, start, start, { msTimeout: 4000 });
-})();
+FontDetect.onFontLoaded(CONFIG.fontFamily, start, start, { msTimeout: 5000 });
+};
